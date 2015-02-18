6c92b5fa9f7ea73d5689f8d3b68b4f74ed1cf0a0
Neoceratodus <Neoceratodus@cichlid3.biologie.uni-konstanz.de>
2015-02-18 11:37:14 +0100

    new script

---
+++ rm_gap_only_seqs.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+ 
+use strict;
+use Bio::SeqIO;
+use Data::Dumper;
+
+# Iker Irisarri. University of Konstanz, Februrary 2015
+
+my $usage = "rm_gap_only_seqs.pl in_fasta > stdout (fasta)\n";
+my $fasta = $ARGV[0] or die $usage; 
+
+# file in with seqio
+
+#READ_IN: 
+my $seqio_obj = Bio::SeqIO->new('-file' => "<$fasta", 
+				'-format' => "fasta", 
+				'-alphabet' => "protein"
+				);
+
+# store sequences in a hash
+
+my %hash;
+
+while (my $inseq = $seqio_obj->next_seq) {
+
+    if ( $inseq->seq =~ /^[-XN]*$/ ) {
+	
+	print STDERR $inseq->primary_id, " contains only undetermined characters\n"; 
+
+    }
+
+    else {
+
+	print ">", $inseq->primary_id, "\n";
+	print $inseq->seq, "\n";
+
+    }
+}
