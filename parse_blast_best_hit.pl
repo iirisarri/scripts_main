551cee9bb228e173df5e991ea9d7e0853b4613f9
iirisarri <irisarri.iker@gmail.com>
2016-01-22 17:34:16 +0100

    slight modif for parsing query and hit names

--- parse_blast_best_hit.pl
+++ parse_blast_best_hit.pl
@@ -10,15 +10,15 @@ use Data::Dumper;
 # option to use a filter for an evalue threshold
 # Iker Irisarri. Konstanz, November 2014
 # originally for getting comps of best hits to extract from fasta files (transcriptomes)
-
+# modif Jan 2016 to make it general
 
 my $usage = "parse_blast_best_hit.pl blast_report > output.fa";
 my $blast_report = $ARGV[0] or die $usage;
 
 # read blast report
-my $report = new Bio::SearchIO(-format => "blast", 
-                           -file   => "<$blast_report",
-                          );
+my $report = new Bio::SearchIO(	-format => "blast", 
+                           		-file   => "<$blast_report"
+                          		);
 
 my %hash;
 # initialize evalue as 1
@@ -32,29 +32,30 @@ while( my $result = $report->next_result ) {
 
     while( my $hit = $result->next_hit ) {
 
-	# get query name
-	$result->query_name =~ /Protopteru_(G\d{5})/;
-        my $query=$1;
-	# get evalue for that hit
-        my $evalue = $hit->significance;
+		# get query name
+		# $result->query_name =~ /Protopteru_(G\d{5})/;
+		#    my $query=$1;
+		my $query = $result->query_name;
+		# get evalue for that hit
+		my $evalue = $hit->significance;
 
-	# compare evalue of this hit with that from previous hits
-	# for first hit, compares against evalue of 1
-	# for next hits, compares with evalue of previous hit
-	if ( $evalue < $evalue1 ) {
+		# compare evalue of this hit with that from previous hits
+		# for first hit, compares against evalue of 1
+		# for next hits, compares with evalue of previous hit
+		if ( $evalue < $evalue1 ) {
 
-	    # assing new evalue to $value1 (for comparison)
-	    $evalue1 = $evalue;
+			# assing new evalue to $value1 (for comparison)
+			$evalue1 = $evalue;
 
-	    # get info and store it in array
-#	    my $hit_name = $hit->name;
-	    # to remove "lcl|" that appears in hit name
-	    $hit->name =~ /lcl\|(.+)/;
-	    my $hit_name = $1;
-	    my $bits1 = $hit->bits;
+			# get info and store it in array
+			my $hit_name = $hit->name;
+			# to remove "lcl|" that appears in hit name
+			# $hit->name =~ /lcl\|(.+)/;
+			# my $hit_name = $1;
+			my $bits1 = $hit->bits;
 
-	    $hash{$query} = [$hit_name,$bits1,$evalue];
-	}
+			$hash{$query} = [$hit_name,$bits1,$evalue];
+		}
     }
 }
 
