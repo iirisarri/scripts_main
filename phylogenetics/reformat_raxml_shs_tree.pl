28e4501eefa91326b88ef4d303eff7b0e7135140
iirisarri <irisarri.iker@gmail.com>
2016-07-24 13:16:24 +0200

    change pattern match for IC

--- phylogenetics/reformat_raxml_shs_tree.pl
+++ phylogenetics/reformat_raxml_shs_tree.pl
@@ -6,7 +6,8 @@ use strict;
 # reformat_raxml_shs_tree.pl
 # Iker Irisarri. University of Konstanz. Jan 2016
 # Change tree format after computation of SH-like support values by RAxML to be read by tree editors
-
+# July 2016: pattern match change to allow reformatting IC trees (after manually removing either IC or ICA:
+# e.g. by replacing "\[-*\d\.\d+," by "[" or ",\-*\d\.\d+]" by "]"
 
 my $usage = "reformat_raxml_shs_tree.pl infile.tre > stdout\n";
 my $infile = $ARGV[0] or die $usage;
@@ -21,7 +22,12 @@ while ( my $line =<IN> ) {
 	$tree = $line;
 
 	# swap places for branch lengths and support values
-	$tree =~ s/(:\d*\.\d*)(\[\d+])/$2$1/g;
+	
+	# modified for shs and IC trees
+	$tree =~ s/(:\d*\.\d*)(\[[-\d\.]+])/$2$1/g;
+	# original for shs trees
+	#$tree =~ s/(:\d*\.\d*)(\[\d+])/$2$1/g;
+	
 	# remove square brackets
 	$tree =~ s/\[//g;
 	$tree =~ s/\]//g;
